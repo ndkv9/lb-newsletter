ea198a370b8fe109ce072f53ba14c2e1fb1539dc
Vu Nguuyen <vu.nguyen101@.com>
2022-01-28 10:33:26 +0200

    Info: initial state

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Lead from './components/Lead'
+
+function App() {
+  return (
+   <div>
+     <h3>
+       Imma Lead
+       <Lead />
+     </h3>
+   </div>
+  );
+}
+
+export default App;
