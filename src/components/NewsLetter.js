bac5c91b291af5a7cc6a404bad84e7160cafeffa
Vu Nguuyen <vu.nguyen101@.com>
2022-01-31 14:41:10 +0200

    Modify styles

--- src/components/NewsLetter.js
+++ src/components/NewsLetter.js
@@ -6,47 +6,88 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
 import CancelIcon from '@mui/icons-material/Cancel';
 import { makeStyles } from '@material-ui/core/styles'
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#424242',
+      main: '#212121',
+      dark: '#000',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#d80711',
+      dark: '#f44336',
+      contrastText: '#fff',
+    },
+  },
+});
 
 const useStyles = makeStyles({
-  toolbar: {
-    backgroundColor: "#000",
+  field: {
+    backgroundColor: "#fff",
+    color: "#000",
+    borderRadius: "5px",
   },
   btn: {
-    backgroundColor: "#F00",
-    color: "#000",
+    height: "48px",
   },
   cancelBtn: {
-    marginLeft: "30px"
-}
+    marginLeft: "10px",
+  }
 })
 
 function NewsLetter() {
   const classes = useStyles()
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <ThemeProvider theme={theme}>
       <AppBar position="static" >
-        <Toolbar className={classes.toolbar}>
+        <Toolbar >
           <Stack direction="row" spacing={2}>
             <Avatar alt="logo" src="https://stripe-images.s3.us-west-1.amazonaws.com/works-with/0bf92fa8419e3237a6fb6eac9b1c225099f96741"/>
           </Stack>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} align="center">
             Need more lead? Try our bots and forms
           </Typography>
-          <Button color="inherit" >Subscribe</Button>
-          <Stack direction="row" spacing={1} className={classes.cancelBtn}>
-            <IconButton aria-label="fingerprint" color="inherit">
-              <CancelIcon />
+          <Box
+            component="form"
+            sx={{
+              '& .MuiTextField-root': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+            marginRight="10px"
+          >
+            <div>
+              <TextField
+                id="standard-textarea"
+                label="Email"
+                placeholder="Placeholder"
+                multiline
+                variant="filled"
+                size="small"
+                className={classes.field}
+              />
+            </div>
+          </Box>
+          <Button variant="contained" color="secondary" className={classes.btn}>Submit</Button>
+          <Stack direction="row" spacing={1} >
+            <IconButton aria-label="fingerprint" color="inherit" >
+              <CancelIcon className={classes.cancelBtn}/>
             </IconButton>
           </Stack>
         </Toolbar>
       </AppBar>
-    </Box>
-
+    </ThemeProvider>
   )
 }
 
